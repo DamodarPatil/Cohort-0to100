160af30d1fdb59e0acf1a814193780ed67cfd092
DamodarPatil <patildamodar1983@gmail.com>
2024-09-23 14:59:21 +0530

    Refactor user update function and remove unnecessary code

--- Week 10.2 - Prisma ORM/src/index.ts
+++ Week 10.2 - Prisma ORM/src/index.ts
@@ -2,30 +2,30 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// const insertUser = async (
-//   email: string,
-//   password: string,
-//   firstName: string,
-//   lastName: string
-// ) => {
-//   const response = await prisma.user.create({
-//     data: {
-//       email,
-//       password,
-//       firstName,
-//       lastName,
-//     },
-//     select: {
-//       id: true,
-//       password: true,
-//       email: true,
-//       firstName: true,
-//       lastName: true,
-//     },
-//   });
-//   console.log(response);
-// };
-// insertUser("damodar3@gmail.com", "password", "Damodar", "Patil");
+const insertUser = async (
+  email: string,
+  password: string,
+  firstName: string,
+  lastName: string
+) => {
+  const response = await prisma.user.create({
+    data: {
+      email,
+      password,
+      firstName,
+      lastName,
+    },
+    select: {
+      id: true,
+      password: true,
+      email: true,
+      firstName: true,
+      lastName: true,
+    },
+  });
+  console.log(response);
+};
+insertUser("damodar3@gmail.com", "password", "Damodar", "Patil");
 
 interface UpdateParams {
   firstName: string;
@@ -49,3 +49,11 @@ updateUser("damodar@gmail.com", {
   firstName: "Moon",
   lastName: "Due",
 });
+
+const getUser = async (email: string) => {
+  const response = await prisma.user.findFirst({
+    where: { email },
+  });
+  console.log(response);
+};
+getUser("damodar1@gmail.com");
