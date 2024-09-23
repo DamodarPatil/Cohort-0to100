25b340f7a2435abd8074edd5a52541d2142b5ee6
DamodarPatil <patildamodar1983@gmail.com>
2024-09-23 14:19:45 +0530

    Refactor user update function and remove unnecessary code

--- Week 10.2 - Prisma ORM/src/index.ts
+++ Week 10.2 - Prisma ORM/src/index.ts
@@ -2,23 +2,49 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const insertUser = async (
+// const insertUser = async (
+//   email: string,
+//   password: string,
+//   firstName: string,
+//   lastName: string
+// ) => {
+//   const response = await prisma.user.create({
+//     data: {
+//       email,
+//       password,
+//       firstName,
+//       lastName,
+//     },
+//     select: {
+//       id: true,
+//       password: true,
+//       email: true,
+//       firstName: true,
+//       lastName: true,
+//     },
+//   });
+//   console.log(response);
+// };
+// insertUser("damodar3@gmail.com", "password", "Damodar", "Patil");
+
+interface UpdateParams {
+  firstName: string;
+  lastName: string;
+}
+
+const updateUser = async (
   email: string,
-  password: string,
-  firstName: string,
-  lastName: string
+  { firstName, lastName }: UpdateParams
 ) => {
-  const response = await prisma.user.create({
+  prisma.user.update({
+    where: { email },
     data: {
-      email,
-      password,
       firstName,
       lastName,
     },
-    select: {
-      id: true,
-      password: true,
-    },
   });
-  console.log(response);
 };
+updateUser("damodar@gmail.com", {
+  firstName: "john",
+  lastName: "due",
+});
